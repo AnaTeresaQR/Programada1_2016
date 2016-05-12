8b173cfcbd2627d399f87b29a7ced170e6891e53
AnaTeresaQR <ana.quesada@ucrso.info>
2016-05-11 18:55:01 -0600

    Login
    se hace login correctamente de un usaurio

--- TP1__B35473_B26178_B05044/src/managerList/Main.java
+++ TP1__B35473_B26178_B05044/src/managerList/Main.java
@@ -18,14 +18,16 @@ public class Main {
 
         Director director = new Director();
 
-        User user3 = director.createUser(abs, "207670378", "Chester", "chester7@ucrso.info", "chester123", new GregorianCalendar(1995, 9, 9), "84590688");
+        User user3 = director.createUser(abs, "207410170", "AnaTeresa", "ana.quesada@ucrso.info", "ana123", new GregorianCalendar(1995, 9, 9), "84590688");
         UsersList userList = new UsersList();
-        // Guardamos la lista
+        // Guardamos la list
+        userList.register(user3);
         userList.save();
         // Se refresca
         userList.refresh();
 
         System.out.println(userList.getIndex(0));
+        System.out.println("Login user: " + userList.login("ana.quesada@ucrso.info", "ana123"));
         System.out.println("");
 
     }
